e2cf4dff3537043ff6148e9dd7d8b9eff3a52db5
mk-pmb <mk-pmb@users.noreply.github.com>
2016-08-22 17:14:40 +0200

    file lists

---
+++ __meta__/files.upd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# -*- coding: utf-8, tab-width: 2 -*-
+
+function upd_flist () {
+  local SELFFILE="$(readlink -m "$BASH_SOURCE")"
+  local SELFPATH="$(dirname "$SELFFILE")"
+  cd "$SELFPATH"/.. || return $?
+  EZGAL_LISTFN="${SELFFILE%.upd.sh}" ezgal3ry-update-file-lists Humanity*
+  return 0
+}
+
+upd_flist "$@"; exit $?
